0b822a6707486061782b3bf9939ea633fc4c243a
Yermak0303 <45458585+Yermak0303@users.noreply.github.com>
2019-03-15 14:29:39 +0200

    Add files via upload

---
+++ src/laba3/pkg2/Laba32.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laba3.pkg2;
+
+/**
+ *
+ * @author 1710511
+ */
+public class Laba32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int n = 3;
+        int[][] a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = n*i + j;
+            }
+        }
+ 
+        
+        System.out.println("Начальная матрица");
+        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%4d", a[i][j]);
+            }
+            System.out.println();
+        }
+ 
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+            }
+        }
+ 
+        
+        System.out.println();
+        System.out.println("Транспонированая матрица");
+        System.out.println("------");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%3d", a[i][j]);
+            }
+            System.out.println();
+       
+    }
+    
+}
+}
